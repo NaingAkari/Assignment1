e1a8c8f837267156687adad4f8cf60745f259ebd
NaingAkari <yingying23510@gmail.com>
2019-02-26 12:20:55 +0630

    First Commit

---
+++ Assignment1/Controllers/CalendarController.cs
@@ -0,0 +1,26 @@
+﻿using Assignment1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+            using (AssignmentMEntities dc = new AssignmentMEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
---
+++ Assignment1/Controllers/CollectsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assignment1;
+
+namespace Assignment1.Controllers
+{
+    public class CollectsController : Controller
+    {
+        private AssignmentMEntities db = new AssignmentMEntities();
+
+        // GET: Collects
+        public ActionResult Index()
+        {
+            return View(db.Collects.ToList());
+        }
+
+        // GET: Collects/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collect collect = db.Collects.Find(id);
+            if (collect == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collect);
+        }
+
+        // GET: Collects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Collects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FullName,Email,Feedback")] Collect collect)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Collects.Add(collect);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(collect);
+        }
+
+        // GET: Collects/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collect collect = db.Collects.Find(id);
+            if (collect == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collect);
+        }
+
+        // POST: Collects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FullName,Email,Feedback")] Collect collect)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(collect).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(collect);
+        }
+
+        // GET: Collects/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collect collect = db.Collects.Find(id);
+            if (collect == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collect);
+        }
+
+        // POST: Collects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Collect collect = db.Collects.Find(id);
+            db.Collects.Remove(collect);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
